835fbf059b7664f30835b1ea23fb92d1067cd6ad
JuniorFerreira <109568117+Juni0rFerreira@users.noreply.github.com>
2023-02-19 18:10:27 -0300

    Commit

--- lib/Modules/Login/screen/recover_password.dart
+++ lib/Modules/Login/screen/recover_password.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mercado/Controllers/login_controller.dart';
+import 'package:mercado/Modules/Login/widgets/input_text.dart';
 
 class RecoverPassword extends GetView<RecoverPasswordController> {
   const RecoverPassword({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<RecoverPasswordController>(builder: (_) {
-      return const Scaffold();
-    });
+    return GetBuilder<RecoverPasswordController>(
+      builder: (_) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Icon(
+                      Icons.email_outlined,
+                      size: 100,
+                      color: Colors.green,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Text(
+                      'Email Address',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  InputText(
+                    obscure: false,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
